58362076de521889e4fd89a56d0488ff4ff9d330
rakshi059 <rakshi059@gmail.com>
2019-09-18 11:25:43 +0530

    work in progress

--- handlers/authors.js
+++ handlers/authors.js
@@ -1,19 +1,22 @@
 const { Author } =  require("@quintype/backend");
 const { DEFAULT_STORY_FIELDS } = require("../constants/constants");
 
+/**
+ * * This Handler is for getting the childItem for authors
+ */
 async function authorsRequestHandler(req, res, next, { client }) {
     const authorId = req.query["id"];
     const limit  = req.query["limit"] || 20;
     const offset = req.query["offset"] || 0;
-    const storyfields = (req.query["story-fields"] && req.query["story-fields"].split(",")) || null;
+    const storyfields = (req.query["story-fields"] && req.query["story-fields"].split(",")) || DEFAULT_STORY_FIELDS;
 
-    const authorCollection = await Author.getAuthorCollection(client, authorId , {limit,  offset , storyfields : storyfields})
-    const stories = authorCollection.items.map(item => getAuthorStory(item));
+    const authorCollection = await Author.getAuthorCollection(client, authorId , {limit,  offset , "story-fields" : storyfields})
+    const stories = authorCollection.items.map(item => getRefactoredStoryObject(item.story));
 
-    const authorsData = { 
+    const authorsData = {
         "total-count" : authorCollection["total-count"],
         "template" : authorCollection["template"],
-        "items" : stories 
+        "items" : stories
     };
 
     res
@@ -22,8 +25,40 @@ async function authorsRequestHandler(req, res, next, { client }) {
     .json(authorsData);
   }
 
-  function getAuthorStory(item){
-    const story = item.story;
+/**
+ * This Handler is for getting the childItem for advanced-search
+ */
+  async function advancedSearchRequestHandler(req, res, next, { client }) {
+    console.log(client)
+    const searchParam = req.query["q"];
+    const limit  = req.query["limit"] || 20;
+    const offset = req.query["offset"] || 0;
+    const storyfields = (req.query["story-fields"] && req.query["story-fields"].split(",")) || DEFAULT_STORY_FIELDS;
+
+    const advancedSearch = await client.getAdvancedSearch({
+        q : searchParam,
+        limit:limit,
+        offset:offset,
+        "content-types": "story",
+        "story-fields":storyfields
+    })
+    console.log("advancedSearch is ==== " + JSON.stringify(advancedSearch));
+    console.log("query param is " + searchParam);
+    const advancedSearchStories = advancedSearch.items.map(story => getRefactoredStoryObject(story));
+    const advancedSearchData = {
+        "total-count" : advancedSearch["total"],
+        "items" : advancedSearchStories
+    };
+
+    res
+    .header("Cache-Control", "public,max-age=15,s-maxage=900,stale-while-revalidate=7200,stale-if-error=14400")
+    .header("Vary", "Accept-Encoding")
+    .json(advancedSearchData);
+  }
+
+  function getRefactoredStoryObject(story){
+  console.log("story from advanced search is ==== " + story.slug);
+
     const refactoredStoryObject = {
         "type" : "story",
         "id" : story["id"],
@@ -41,5 +76,5 @@ async function authorsRequestHandler(req, res, next, { client }) {
     }
     return refactoredStoryObject;
   }
-  
-  module.exports = { authorsRequestHandler };
+
+  module.exports = { authorsRequestHandler, advancedSearchRequestHandler };
--- index.js
+++ index.js
@@ -2,5 +2,6 @@ const { collectionHandler } = require("./handlers/collection-data");
 const { appConfigRequestHandler } = require("./handlers/app-config");
 const { mobileMenuRequestHandler } = require("./handlers/menu-groups");
 const { authorsRequestHandler } = require("./handlers/authors");
+const { advancedSearchRequestHandler } = require("./handlers/authors");
 
-module.exports = {collectionHandler , appConfigRequestHandler , mobileMenuRequestHandler , authorsRequestHandler};
+module.exports = {collectionHandler , appConfigRequestHandler , mobileMenuRequestHandler , authorsRequestHandler, advancedSearchRequestHandler};
